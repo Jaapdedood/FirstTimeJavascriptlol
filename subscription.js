c65e41da451316b7bb71e614ae3b4dbc1961ce67
Jaap de Dood <jaapdd@gmail.com>
2020-07-17 10:15:05 -0700

    fixes

--- subscription.js
+++ subscription.js
@@ -1,3 +1,5 @@
+var showResults;
+
 let filterCoffeeMap = new Map([
   ['light', 0],
   ['medium', 0],
@@ -24,9 +26,9 @@ function answer1(input, choice) {
   if (next.style.display === "none") {
     next.style.display = "block";
   }
-  if(showResults === true){
-    updateResults();
-  }
+    if(showResults === true){
+        updateResults();
+    }
 
   switch(choice) {
     case 1:
@@ -45,7 +47,7 @@ function answer1(input, choice) {
       document.getElementById("option3").style.textDecoration = "underline";
       break;
   }
-	
+
 	next.scrollIntoView({behavior: "smooth"});
 }
 
@@ -78,15 +80,15 @@ function answer2(input, choice) {
     case 6:
       document.getElementById("option4").style.textDecoration = "none";
       document.getElementById("option5").style.textDecoration = "none";
-      document.getElementById("option6").style.textDecoration = "none";
+      document.getElementById("option6").style.textDecoration = "underline";
       document.getElementById("option7").style.textDecoration = "none";
       document.getElementById("option8").style.textDecoration = "none";
       break;
     case 7:
       document.getElementById("option4").style.textDecoration = "none";
       document.getElementById("option5").style.textDecoration = "none";
-      document.getElementById("option6").style.textDecoration = "underline";
-      document.getElementById("option7").style.textDecoration = "none";
+      document.getElementById("option6").style.textDecoration = "none";
+      document.getElementById("option7").style.textDecoration = "underline";
       document.getElementById("option8").style.textDecoration = "none";
       break;
     case 8:
@@ -97,7 +99,6 @@ function answer2(input, choice) {
       document.getElementById("option8").style.textDecoration = "underline";
       break;
   }
-	
 	next.scrollIntoView({behavior: "smooth"});
 }
 
@@ -111,44 +112,43 @@ function answer3(input, choice) {
     updateResults();
   }
 
-  switch(choice) {
-    case 4:
-      // loops? this-> ? nahh
-      document.getElementById("option9").style.textDecoration = "underline";
-      document.getElementById("option10").style.textDecoration = "none";
-      document.getElementById("option11").style.textDecoration = "none";
-      document.getElementById("option12").style.textDecoration = "none";
-      document.getElementById("option13").style.textDecoration = "none";
-      break;
-    case 5:
-      document.getElementById("option9").style.textDecoration = "none";
-      document.getElementById("option10").style.textDecoration = "underline";
-      document.getElementById("option11").style.textDecoration = "none";
-      document.getElementById("option12").style.textDecoration = "none";
-      document.getElementById("option13").style.textDecoration = "none";
-      break;
-    case 6:
-      document.getElementById("option9").style.textDecoration = "none";
-      document.getElementById("option10").style.textDecoration = "none";
-      document.getElementById("option11").style.textDecoration = "none";
-      document.getElementById("option12").style.textDecoration = "none";
-      document.getElementById("option13").style.textDecoration = "none";
-      break;
-    case 7:
-      document.getElementById("option9").style.textDecoration = "none";
-      document.getElementById("option10").style.textDecoration = "none";
-      document.getElementById("option11").style.textDecoration = "underline";
-      document.getElementById("option12").style.textDecoration = "none";
-      document.getElementById("option13").style.textDecoration = "none";
-      break;
-    case 8:
-      document.getElementById("option9").style.textDecoration = "none";
-      document.getElementById("option10").style.textDecoration = "none";
-      document.getElementById("option11").style.textDecoration = "none";
-      document.getElementById("option12").style.textDecoration = "none";
-      document.getElementById("option13").style.textDecoration = "underline";
-      break;
+    switch(choice) {
+    case 9:
+        // loops? this-> ? nahh
+        document.getElementById("option9").style.textDecoration = "underline";
+        document.getElementById("option10").style.textDecoration = "none";
+        document.getElementById("option11").style.textDecoration = "none";
+        document.getElementById("option12").style.textDecoration = "none";
+        document.getElementById("option13").style.textDecoration = "none";
+        break;
+    case 10:
+        document.getElementById("option9").style.textDecoration = "none";
+        document.getElementById("option10").style.textDecoration = "underline";
+        document.getElementById("option11").style.textDecoration = "none";
+        document.getElementById("option12").style.textDecoration = "none";
+        document.getElementById("option13").style.textDecoration = "none";
+        break;
+    case 11:
+        document.getElementById("option9").style.textDecoration = "none";
+        document.getElementById("option10").style.textDecoration = "none";
+        document.getElementById("option11").style.textDecoration = "underline";
+        document.getElementById("option12").style.textDecoration = "none";
+        document.getElementById("option13").style.textDecoration = "none";
+        break;
+    case 12:
+        document.getElementById("option9").style.textDecoration = "none";
+        document.getElementById("option10").style.textDecoration = "none";
+        document.getElementById("option11").style.textDecoration = "none";
+        document.getElementById("option12").style.textDecoration = "underline";
+        document.getElementById("option13").style.textDecoration = "none";
+        break;
+    case 13:
+        document.getElementById("option9").style.textDecoration = "none";
+        document.getElementById("option10").style.textDecoration = "none";
+        document.getElementById("option11").style.textDecoration = "none";
+        document.getElementById("option12").style.textDecoration = "none";
+        document.getElementById("option13").style.textDecoration = "underline";
+        break;
   }
-	
 	next.scrollIntoView({behavior: "smooth"});
 }
